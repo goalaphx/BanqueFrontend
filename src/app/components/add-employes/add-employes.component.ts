cca3f04d5f189fbf5345e6cff3d1236354e91ea5
khalilh2002 <khalil.houssein2002@gmail.com>
2024-11-10 22:40:15 +0100

    chof hadi

--- src/app/components/add-employes/add-employes.component.ts
+++ src/app/components/add-employes/add-employes.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Employe } from '../../models/employe.model';
+import { EmployeService } from '../../services/employe.service';
+import { NgbModal, NgbModalConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-add-employes',
@@ -8,5 +11,30 @@ import { Component } from '@angular/core';
   styleUrl: './add-employes.component.css'
 })
 export class AddEmployesComponent {
+  newEmploye: Employe = { codeEmploye: 0, nomEmploye: '' };
+  error: string | null = null;
+
+
+  constructor(private employeeService: EmployeService , private modalService : NgbModal , config : NgbModalConfig) {}
+
+
+
+  submitClient(): void {
+
+
+    // this.employeeService.(this.newEmploye).subscribe({
+    //   next: () => {
+    //     alert('added succesfuly')
+    //     this.newEmploye = { codeEmploye: 0, nomEmploye: '' }; // Reset form
+    //   },
+    //   error: (err) => {
+    //     this.error = 'Failed to add client';
+    //     console.error(err);
+    //   }
+    // });
+  }
+  open(content:any) {
+		this.modalService.open(content);
+	}
 
 }
