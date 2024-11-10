1d9bdb61a4f179378165d1cc2acfe26969d29415
El Mahdi Id Lahcen <idlahcen.elmahdi@etu.uae.ac.ma>
2024-11-10 22:56:00 +0100

    test

---
+++ src/app/components/operation-list/operation-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-operation-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './operation-list.component.html',
+  styleUrl: './operation-list.component.css'
+})
+export class OperationListComponent {
+
+}
