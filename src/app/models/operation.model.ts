5b47977f92daf5cec51da908b446a2762c4d2e25
El Mahdi Id Lahcen <idlahcen.elmahdi@etu.uae.ac.ma>
2024-11-12 01:23:54 +0100

    fixed something

--- src/app/models/operation.model.ts
+++ src/app/models/operation.model.ts
@@ -2,6 +2,7 @@ import { Compte } from './compte.model';
 import { Employe } from './employe.model';
 
 export interface Operation {
+  type: string;
   numeroOperation: number;
   dateOperation: Date;
   montant: number;
